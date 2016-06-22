5045350bc68c46cfbad810c0201f9d5ccf95fedf
jruel006 <jruel006@ucr.edu>
2016-06-22 11:39:26 -0700

    Added custom functionality and disabled editing on tips views

--- TipPro/ViewController.swift
+++ TipPro/ViewController.swift
@@ -8,21 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    @IBOutlet weak var billAmountFiled: UITextField!
+    
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var billAmountField: UITextField!
     @IBOutlet weak var tipSelector: UISegmentedControl!
+    @IBOutlet weak var tipAmountField: UITextField!
+    @IBOutlet weak var totalAmountField: UITextField!
+
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        print ("Button is pressed!")
+        
+        guard let billAmount = Double(billAmountField.text!) else {
+            //show error
+            billAmountField.text = ""
+            tipAmountField.text = ""
+            totalAmountField.text = ""
+            return
+        }
+        
+        var tipPercentage = 0.0
+        
+        switch tipSelector.selectedSegmentIndex {
+        case 0:
+            tipPercentage = 0.10
+        case 1:
+            tipPercentage = 0.15
+        case 2:
+            tipPercentage = 0.20
+        default:
+            break
+        }
+        
+        let roundedBillAmount = round(100*billAmount)/100
+        let tipAmount = roundedBillAmount * tipPercentage
+        let roundedTipAmount = round(100*tipAmount)/100
+        let totalAmount = roundedBillAmount + roundedTipAmount
+        
+        if (!billAmountField.editing) {
+            billAmountField.text = String(format: "%.2f", roundedBillAmount)
+        }
+        tipAmountField.text = String(format: "%.2f", roundedTipAmount)
+        totalAmountField.text = String(format: "%.2f", totalAmount)
+        
+        print(totalAmountField.text)
+    }
+    
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad() /*This ensures that our view loaded*/
+        self.billAmountField.delegate = self /*we select our text field that we want*/
+        self.billAmountField.keyboardType = UIKeyboardType.NumberPad /*We make sure that the keyboard is a numapad*/
+        
+        self.tipAmountField.userInteractionEnabled = true
+        self.tipAmountField.enabled = false
+        
+        self.totalAmountField.userInteractionEnabled = true
+        self.totalAmountField.enabled = false
+        
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: Selector("dismissKeyboard")))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func dismissKeyboard(){ /*this is a void function*/
+        billAmountField.resignFirstResponder() /*This will dismiss our keyboard*/
     }
 
+    
+
+    
 
 }
 
